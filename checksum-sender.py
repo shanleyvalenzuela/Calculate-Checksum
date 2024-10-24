73057d29fe369d2cc9e0d01635e71e23062d1266
Shanley Valenzuela <146340503+shanleyvalenzuela@users.noreply.github.com>
2024-10-24 22:18:39 +0800

    Update checksum-sender.py

--- checksum-sender.py
+++ checksum-sender.py
@@ -38,12 +38,16 @@ def sender():
 
         # Packet to send
         sent_packet = [7, 11, 12, 0, 6, 0]
+        # data = "PKA_NSCOM03" # uncomment for Example 2
+        # sent_packet = [ord(char) for char in data] # uncomment for Example 2
         bit_size = 4
         packet_with_checksum = create_sender_table(sent_packet, bit_size)
         
         serialized_packet = pickle.dumps(packet_with_checksum)
         s.sendall(serialized_packet)
+        print(f"Sent String: {data}")
         print(f"Sent Packet: {packet_with_checksum}")
+        print(f"Sent Checksum: {packet_with_checksum[len(packet_with_checksum)-1]}")
 
 def main():
         sender()
